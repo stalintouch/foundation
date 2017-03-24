7f622eb1069dcb2b7e39170f0aae34a52a01a0f3
stalintouch <stalintouch1983@gmail.com>
2017-03-23 22:37:52 -0400

    added static pages future projects, graduations, conferences

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,14 @@
 class WelcomesController < ApplicationController
   def index 
   end
+
+  def conferences
+  end
+
+  def graduations
+  end
+
+  def futureprojects
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'welcomes#index'
 
+  get 'welcomes/conferences'
+  get 'welcomes/graduations'
+  get 'welcomes/futureprojects'
+
   resources :projects
 end
